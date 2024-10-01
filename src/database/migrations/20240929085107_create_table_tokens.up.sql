41c29b0a7dfd57eff7e5c533feb00e59c8833169
ananta weda <109273598+AnantaWeda@users.noreply.github.com>
2024-10-01 15:50:53 +0800

    refactor: Replace migration (#8)
    refactor: Replace auto migration with golang-migrate

---
+++ src/database/migrations/20240929085107_create_table_tokens.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tokens(
+    id              UUID            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    token           VARCHAR(255)    NOT NULL,
+    user_id         UUID            NOT NULL UNIQUE,
+    type            VARCHAR(255)    NOT NULL,
+    expires         TIMESTAMP       DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+    created_at      TIMESTAMP       DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+    updated_at      TIMESTAMP       DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
